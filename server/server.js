2d94745aae62dacd0f39c0f6125655fc96e66105
FatimahHaruna <Fatimaharuna2007@gmail.com>
2026-08-25 22:36:03 +0100

    Add category routes to server configuration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
 const app = express();
 
 connectDB();
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
     res.send('Home Page!')
 });
 app.use('/freshcart/products', productRoutes);
+app.use('/freshcart/category', categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 
