873cd0a37e355621b47bdf16587d5ccb4ff6866d
FatimahHaruna <Fatimaharuna2007@gmail.com>
2026-08-27 22:15:12 +0100

    Implement JWT authentication middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, getUserProfile, updateUserProfile } = require('../controllers/authController');
-//const protect = require('../middlewares/authMiddleware');
+const protect = require('../middlewares/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
