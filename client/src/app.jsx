eb5ea04086e133f9d3ca9c8c37c88de4370682c1
FatimahHaruna <Fatimaharuna2007@gmail.com>
2026-09-04 21:56:32 +0100

    Implement routing and layout structure with authentication support in App component

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,15 +1,115 @@
-import Navbar from "./components/Navbar";
-import ProductCard from "./components/ProductCard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App () {
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
+
+import MainLayout from "./layouts/MainLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Orders from "./pages/Orders";
+import OrderDetails from "./pages/OrderDetails";
+import NotFound from "./pages/NotFound";
+
+import "./App.css";
+
+function App() {
   return (
-    <>
-      <Navbar />
-      <ProductCard name='Apple' price={2500} />
-      <ProductCard name='Banana' price={1500} />
-    </>
-     
-  )
+    <BrowserRouter>
+
+      <AuthProvider>
+
+        <CartProvider>
+
+          <MainLayout>
+
+            <Routes>
+
+              <Route path="/" element={<Home />} />
+
+              <Route
+                path="/categories"
+                element={<Categories />}
+              />
+
+              <Route
+                path="/products"
+                element={<Products />}
+              />
+
+              <Route
+                path="/products/:id"
+                element={<ProductDetails />}
+              />
+
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+
+              <Route
+                path="/register"
+                element={<Register />}
+              />
+
+              <Route
+                path="/cart"
+                element={
+                  <ProtectedRoute>
+                    <Cart />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/checkout"
+                element={
+                  <ProtectedRoute>
+                    <Checkout />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/orders"
+                element={
+                  <ProtectedRoute>
+                    <Orders />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/orders/:id"
+                element={
+                  <ProtectedRoute>
+                    <OrderDetails />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="*"
+                element={<NotFound />}
+              />
+
+            </Routes>
+
+          </MainLayout>
+
+        </CartProvider>
+
+      </AuthProvider>
+
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
