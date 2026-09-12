7879b8a450b62a65cee46de75b2c79d96eeec9af
FatimahHaruna <Fatimaharuna2007@gmail.com>
2026-09-12 21:25:17 +0100

    Updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,66 +1,20 @@
-import { Link, useNavigate } from "react-router-dom";
-import useAuth from "../hooks/useAuth";
-import { useCart } from "../context/cartContext.js";
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  const { user, logout } = useAuth();
-  const { itemCount } = useCart();
 
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/");
-  };
-
-  return (
-    <header className="navbar">
-      <div className="navbar-container">
-
-        <Link to="/" className="logo">
-          🛒 FreshCart
-        </Link>
-
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/categories">Categories</Link>
-          <Link to="/products">Products</Link>
-          <Link to="/orders">Orders</Link>
-        </nav>
-
-        <div className="navbar-actions">
-
-          <Link to="/cart" className="cart-link">
-            🛒
-            {itemCount > 0 && (
-              <span className="cart-count">
-                {itemCount}
-              </span>
-            )}
-          </Link>
-
-          {user ? (
-            <>
-              <span>Hello, {user.name}</span>
-
-              <button onClick={handleLogout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/register" className="register-btn">
-                Register
-              </Link>
-            </>
-          )}
-
-        </div>
-
-      </div>
-    </header>
-  );
-};
+function Navbar() {
+    return (
+        <header className='navbar'>
+            <Link to='/' className='logo'>
+              🛒 FreshCart
+            </Link>
+            <nav>
+                <Link>Home</Link>
+                <Link>Categories</Link>
+                <Link>Products</Link>
+                <Link>Order</Link>
+            </nav>
+        </header>
+    )
+}
 
 export default Navbar;
