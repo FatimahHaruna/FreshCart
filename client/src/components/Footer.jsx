a84067ff96cb8d5634ba5fa573a1fdca2516e9d2
FatimahHaruna <Fatimaharuna2007@gmail.com>
2026-09-13 17:23:54 +0100

    Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div>
+                    <h2>🛒FreshCart</h2>
+                    <p>
+                        Fresh fruits and vegetables delivered straight to your doorstep.
+                    </p>
+                </div>
+                <div>
+                    <h3>Quick Links</h3>
+                    <a href='/'>Home</a>
+                    <a href='/'>Categories</a>
+                    <a href='/'>Products</a>
+                </div>
+                <div className="footer-bottom">
+                    © 2026 FreshCart. All rights reserved.
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
